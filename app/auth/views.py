ba6de34c645077c27a1b8c2baa67445ca316ac54
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-20 15:22:54 +0300

    ft: views logic

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,22 @@ from flask_login import login_user, login_required,logout_user,current_user
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('logged in successfully',category='success')
+                login_user(user)
+                return redirect(url_for('main.displayquote'))
+                
+            else:
+                flash('incorrect password,try again!', category='error')
+        else:
+            flash('email does not exist', category='error')
+    return render_template('register.html', user=current_user)
