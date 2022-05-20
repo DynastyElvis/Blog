c3a0074c212d5f8bb0da4c653c48413042ecd8a8
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-05-20 15:12:11 +0300

    ft: logic

--- app/models.py
+++ app/models.py
@@ -27,3 +27,15 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     comment = db.relationship('Comment', lazy='dynamic')
     
+class Comment(db.Model):
+    '''
+    create comment schema for the comment table
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    nickname = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
+    
+    
